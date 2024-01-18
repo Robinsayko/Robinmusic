1a01a6d69779d3e0646d7dedf97a91b2a0847e80
Robinsayko <134005783+Robinsayko@users.noreply.github.com>
2024-01-18 04:52:27 +0300

    Update config.py

--- config.py
+++ config.py
@@ -17,7 +17,8 @@ OWNER_ID = int(getenv("OWNER_ID", "5021621064"))
 PING_IMG = getenv("PING_IMG", "https://te.legra.ph/file/6f99c49bdb4679acad717.jpg")
 START_IMG = getenv("START_IMG", "https://te.legra.ph/file/f8ba75bdbb9931cbc8229.jpg")
 
-SESSION = getenv("SESSION", "BABkb62UqrDuo4zTvpmOd_lSPevEoIqnn3nAT2UdrBMdtfH4jDjpyuWAFGPIyrKpbf2C4Goh72FEsauJ13e_iw1eLPk2gELoR13ERo-H5tCnq9RdkvNW7IxTAfkseF2p8IIRY2AKnPgVbz82CrplszTogghd8OuleOn7mYzh35MtBgBPbcOTz6ZIYeOv6HvgQlOnPgNjky2xzvO38b6T197cJVL4jLBI9UAwrHFRmZOKUaMHRCm-97FJDtnpfW7ogEdrhxqnbhoSjA1bo3JAs0sXnvtWsG8salW-HxQPTi-N0Zv42XDJx87Hh9Q64D5BaLjQKX2WVy_PwMmD3VTakeYYAAAAAWBpS6sA" None)
+SESSION = getenv("SESSION", "BABkb62UqrDuo4zTvpmOd_lSPevEoIqnn3nAT2UdrBMdtfH4jDjpyuWAFGPIyrKpbf2C4Goh72FEsauJ13e_iw1eLPk2gELoR13ERo-H5tCnq9RdkvNW7IxTAfkseF2p8IIRY2AKnPgVbz82CrplszTogghd8OuleOn7mYzh35MtBgBPbcOTz6ZIYeOv6HvgQlOnPgNjky2xzvO38b6T197cJVL4jLBI9UAwrHFRmZOKUaMHRCm-97FJDtnpfW7ogEdrhxqnbhoSjA1bo3JAs0sXnvtWsG8salW-HxQPTi-N0Zv42XDJx87Hh9Q64D5BaLjQKX2WVy_PwMmD3VTakeYYAAAAAWBpS6sA") 
+              
 
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/melodisupport")
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/botDestekKanali")
