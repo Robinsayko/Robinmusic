6904407622be9f8f21c4a99a6df5aa61abb21e72
Robinsayko <134005783+Robinsayko@users.noreply.github.com>
2024-01-18 04:46:34 +0300

    Update config.py

--- config.py
+++ config.py
@@ -19,7 +19,7 @@ START_IMG = getenv("START_IMG", "https://te.legra.ph/file/f8ba75bdbb9931cbc8229.
 
 SESSION = getenv("SESSION", "BABkb62UqrDuo4zTvpmOd_lSPevEoIqnn3nAT2UdrBMdtfH4jDjpyuWAFGPIyrKpbf2C4Goh72FEsauJ13e_iw1eLPk2gELoR13ERo-H5tCnq9RdkvNW7IxTAfkseF2p8IIRY2AKnPgVbz82CrplszTogghd8OuleOn7mYzh35MtBgBPbcOTz6ZIYeOv6HvgQlOnPgNjky2xzvO38b6T197cJVL4jLBI9UAwrHFRmZOKUaMHRCm-97FJDtnpfW7ogEdrhxqnbhoSjA1bo3JAs0sXnvtWsG8salW-HxQPTi-N0Zv42XDJx87Hh9Q64D5BaLjQKX2WVy_PwMmD3VTakeYYAAAAAWBpS6sA" None)
 
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/sohbetsayko")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/melodisupport")
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/botDestekKanali")
 PLAYLIST = getenv("PLAYLIST", "https://t.me/melodiplaylist")
 
